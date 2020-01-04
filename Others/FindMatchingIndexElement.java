eeb8348125f7a08e6c5771c669444f4e1fade2c0
Apoorv <vaporinside@gmail.com>
2020-01-04 21:26:33 +0530

    Started base FindMatchingIndexElement

---
+++ Others/FindMatchingIndexElement.java
@@ -0,0 +1,12 @@
+public class FindMatchingIndexElement{
+
+    static int findMatch(int[] arr){
+        int result = -1;
+        
+        return result;
+    }
+    public static void main(String[] args){
+        int[] arr = {-1, 0, 2, 61, 75 };
+        System.out.println("Common index/element: " + findMatch(arr));
+    }
+}
