ede4af50eeae668c4dfab318c444536799b46842
Apoorv <vaporinside@gmail.com>
2020-01-11 22:18:43 +0530

    Improved GetFactor by limiting check to 1/2 of num

--- Others/AllFact.java
+++ Others/AllFact.java
@@ -1,6 +1,8 @@
+// Finding all factors of a number.
+
 class AllFact{
     static void printFact(int num){
-        for(int i=1; i<num; i++){
+        for(int i=1; i<=num/2; i++){
             if(num%i==0){
                 System.out.println(i);
             }
