e200b382a164c6864cd51e1552588f40b09aae31
Apoorv <vaporinside@gmail.com>
2020-01-13 06:09:34 +0530

    Prime Factors Incomplete

---
+++ Others/PrimeFactor.java
@@ -0,0 +1,27 @@
+class PrimeFactor{
+
+    static void pFact(int num){
+        int x=1;
+        int pcount=0;
+        for(int i=1; i<num/x; i++){
+            if(num%i==0){
+//                num/=i;
+//                x=i;
+                pcount=0;
+                for(int j=1; j<i; j++){
+                    if(i%j==0 && pcount<2){
+                        pcount++;
+                    }
+                    if (pcount > 2) {
+                        break;
+                    }
+                }
+//                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        pFact(100884);
+    }
+}
