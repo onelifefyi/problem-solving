02227d3e9bcec30e034433b92e72c5d732c4d807
Apoorv <vaporinside@gmail.com>
2020-01-16 05:33:16 +0530

    Added ArrayList Data Structure

---
+++ DataStructures/arrList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class arrList{
+    public static void main(String[] args){
+        ArrayList arrLst = new ArrayList();
+        arrLst.add(14);
+        arrLst.add(61);
+        arrLst.add(3);
+        System.out.println(arrLst);
+        System.out.println(arrLst.get(1));
+    }
+}
