15806cecddeb596aa119fb7c37aa59ce8db5c8aa
Apoorv <vaporinside@gmail.com>
2020-01-16 05:35:18 +0530

    Add to index on ArrayList

--- DataStructures/arrList.java
+++ DataStructures/arrList.java
@@ -4,8 +4,9 @@ class arrList{
     public static void main(String[] args){
         ArrayList arrLst = new ArrayList();
         arrLst.add(14);
-        arrLst.add(61);
         arrLst.add(3);
+        System.out.println(arrLst);
+        arrLst.add(1, 61);
         System.out.println(arrLst);
         System.out.println(arrLst.get(1));
     }
