e24c877d1d9001943c7da744a034a5236a72a00d
Apoorv <vaporinside@gmail.com>
2020-02-11 22:31:20 +0530

    Added Problem Diagonal Differece

--- HackerRank/DiagonalDifference.java
+++ HackerRank/DiagonalDifference.java
@@ -1,7 +1,24 @@
 //Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+import java.lang.Math;
 
 class DiagonalDifference{
-    public static int main(String[] args){
-        int arr[][] = 
+
+    static int findDiff(int arr[][]){
+        int result = 0; 
+        int ctr = 0;
+        for(int i=0; i<arr.length; i++){
+            result+=arr[i][i];
+        }
+        for(int i=arr.length - 1; i>=0; i--){
+            result -= arr[i][ctr++];
+        }
+        return Math.abs(result);
+    }
+
+    public static void main(String[] args){
+        int arr[][] = {{1, 2, 3},
+                       {4, 5, 6},
+                       {10, 8, 9}};
+        System.out.print(findDiff(arr));
     }
 }
