01f2279e3eef30ad1846903888d227cf1623a5d9
Apoorv <vaporinside@gmail.com>
2020-02-15 23:37:59 +0530

    Solved ValleyCount

--- HackerRank/ValleyCount.java
+++ HackerRank/ValleyCount.java
@@ -2,5 +2,22 @@
 // calculate the no of valleys(below sea) the hicker encountered
 
 class ValleyCount{
-    
+
+    static int findValley(String path){
+        int noVal = 0;
+        int currAlt = 0;
+
+        for(int i=0; i<path.length(); i++){
+            currAlt += (path.charAt(i) == 'U')?1:-1;
+            if(path.charAt(i) == 'D' && currAlt == -1){
+                noVal++;
+            }
+        }
+        return noVal;
+    }
+
+    public static void main(String[] args){
+        String path = "DDDDDDDDDUUUUUUUUUDDDDDDDDDDDDDDD";
+        System.out.println("No of Valleys: " + findValley(path));
+    }
 }
