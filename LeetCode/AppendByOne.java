bb8972f1059bb3a8b1875c8ecbe913162790476e
Apoorv <vaporinside@gmail.com>
2020-02-24 22:11:58 +0530

    Solved Append Array

--- LeetCode/AppendByOne.java
+++ LeetCode/AppendByOne.java
@@ -1,5 +1,26 @@
 class AppendByOne{
     int[] getAppended(int[] arr){
+        int flag9last = 0;
+        for(int i=arr.length-1; i>=0; i--){
+            if(arr[i]!=9){
+                arr[i]++;
+                break;
+            }
+            else{
+                arr[i] = 0;
+                if(i == 0){
+                    flag9last = 1;
+                }
+            }
+        }
+        if(flag9last==1){
+            int[] newArr = new int[arr.length+1];
+            newArr[0] = 1;
+            for(int j=0; j<arr.length; j++){
+                newArr[j+1] = arr[j];
+            }
+            return newArr;
+        }
         return arr;
     }
 }
@@ -7,7 +28,9 @@ class AppendByOne{
 class Solution{
     public static void main(String[] args){
         AppendByOne obj = new AppendByOne();
-        int[] arr = {1,2,3};
-        System.out.println(obj.getAppended(arr));
+        int[] arr = {9,9,9};
+        for(int ele: obj.getAppended(arr)){
+            System.out.print(ele+ " ");
+        }
     }
 }
