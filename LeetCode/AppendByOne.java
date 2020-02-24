27f391880bcceae4ae2242812eb9e9c09dfd5f95
Apoorv <vaporinside@gmail.com>
2020-02-24 21:57:26 +0530

    Added LeetCode problem

--- LeetCode/AppendByOne.java
+++ LeetCode/AppendByOne.java
@@ -1,2 +1,13 @@
-    System.out.println(appended(arr));
+class AppendByOne{
+    int[] getAppended(int[] arr){
+        return arr;
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+        AppendByOne obj = new AppendByOne();
+        int[] arr = {1,2,3};
+        System.out.println(obj.getAppended(arr));
+    }
 }
