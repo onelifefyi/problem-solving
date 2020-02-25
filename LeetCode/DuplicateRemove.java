7308d461cb2a058bcba2288b21fdc14367c482b5
Apoorv Chandrakar <vaporinside@gmail.com>
2020-02-25 21:33:02 +0530

    Create DuplicateRemove.java

---
+++ LeetCode/DuplicateRemove.java
@@ -0,0 +1,18 @@
+class DuplicateRemove{
+    static int removeDuplicate(int[] arr){
+        int count = arr.length;
+
+        for(int)
+        return count;
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+        int[] arr = {0, 1, 1, 2, 3, 3, 4};
+        int noEle = removeDuplicate(arr);
+        for(int i=0; i<noEle; i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
