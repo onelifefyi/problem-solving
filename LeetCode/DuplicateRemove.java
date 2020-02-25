1f907a8cf0617cedc970b2b1f0a78ab5f0858fca
Apoorv Chandrakar <vaporinside@gmail.com>
2020-02-25 21:47:42 +0530

    Update DuplicateRemove.java
    Completed Problem, Still can be optimized.

--- LeetCode/DuplicateRemove.java
+++ LeetCode/DuplicateRemove.java
@@ -2,15 +2,27 @@ class DuplicateRemove{
     static int removeDuplicate(int[] arr){
         int count = arr.length;
 
-        for(int)
+        for(int i = 0; i<arr.length-1; i++){
+            for(int j = i; j<arr.length; j++){
+                if(arr[j]>arr[i]){
+                    arr[i+1] = arr[j];
+                    break;
+                }
+            }
+        }
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]<=arr[i+1]){
+                count = i;
+            }
+        }
         return count;
     }
 }
 
-class Solution{
+public class Solution{
     public static void main(String[] args){
         int[] arr = {0, 1, 1, 2, 3, 3, 4};
-        int noEle = removeDuplicate(arr);
+        int noEle = DuplicateRemove.removeDuplicate(arr);
         for(int i=0; i<noEle; i++){
             System.out.print(arr[i]+ " ");
         }
