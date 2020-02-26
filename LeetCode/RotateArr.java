97e97ac45fb6939a3c8ebcf2e85ae4441467634f
Apoorv Chandrakar <vaporinside@gmail.com>
2020-02-26 22:20:55 +0530

    Create RotateArr.java

---
+++ LeetCode/RotateArr.java
@@ -0,0 +1,18 @@
+class RotateArr{
+    public void rotate(int[] nums, int k) {
+        int temp;
+        for(int i = 0; i<k; i++){
+            temp = nums[nums.length-1];
+            for(int j=nums.length-1; j>0; j--){
+                nums[j] = nums[j-1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+        
+    }
+}
