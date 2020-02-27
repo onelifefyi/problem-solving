6b1cc42da2a4e5b3406d9e1c70bff0a6b3aedebc
Apoorv Chandrakar <vaporinside@gmail.com>
2020-02-27 20:57:29 +0530

    Completed FindUnique

--- LeetCode/FindUnique.java
+++ LeetCode/FindUnique.java
@@ -1,3 +1,17 @@
 class FindUnique{
-    int 
+    int getUnique(int[] arr){
+        int xSum = 0;
+        for(int elem: arr){
+            xSum ^= elem;
+        }
+        return xSum;
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+        int[] arr = {1, 4, 1, 42, 4};
+        FindUnique obj = new FindUnique(arr);
+        System.out.print("Unique no is: " + getUnique());
+    }
 }
