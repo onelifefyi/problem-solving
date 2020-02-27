008e1181860a8d6f88e0bcd84bc49c23c0c695bc
Apoorv Chandrakar <vaporinside@gmail.com>
2020-02-27 20:58:29 +0530

    Update FindUnique.java
    Added Question Documentation

--- LeetCode/FindUnique.java
+++ LeetCode/FindUnique.java
@@ -1,3 +1,5 @@
+/* Given a non-empty array of integers, every element appears twice except for one. Find that single one. */
+
 class FindUnique{
     int getUnique(int[] arr){
         int xSum = 0;
