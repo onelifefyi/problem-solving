934e08dca63455622d96ca21547a605278573da1
Apoorv Chandrakar <vaporinside@gmail.com>
2020-03-04 08:04:24 +0530

    Create ContainsDuplicate.java
    Added Problem Doc

---
+++ LeetCode/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+/* Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct. */
+
+class ContainsDuplicate{
+    void checkDuplicate(int[] arr){
+
+    }
+}
+
+class Solution{
+    public static void main(String[] args){
+
+    }
+}
+
