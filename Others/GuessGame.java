b78f4b311347fbca63ddc88e389b0045e9ec107f
Apoorv Chandrakar <vaporinside@gmail.com>
2020-03-12 23:10:38 +0530

    Documented & Proper template

---
+++ Others/GuessGame.java
@@ -0,0 +1,14 @@
+class GuessGame{
+    int GetTries(){
+        int tries = 0;
+// Logic Here
+        return tries;
+    }
+}
+
+class Solution{
+    public static void main(String args[]){
+        GuessGame obj = new GuessGame();
+        System.out.print("No of guesses: " + obj.GetTries());
+    }
+}
