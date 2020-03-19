c3ac54e1caf9dd34e2e3f1569aa98655f208d564
Apoorv <vaporinside@gmail.com>
2020-03-19 21:22:41 +0530

    Solved EquializeArray - HackerRank

---
+++ HackerRank/EquilizeArray.java
@@ -0,0 +1,25 @@
+
+
+class Solution{
+    int getMinDel(int[] arr){
+        int[] count = new int[101];
+        for(int ele: arr){
+            count[ele] += 1;
+        }
+        int max = 0;
+        for(int ele: arr){
+            if(count[ele]>max){
+                max = count[ele];
+            }
+        }
+        return arr.length - max;
+    }
+}
+
+class EqualizeArray{
+    public static void main(String[] args){
+        Solution obj = new Solution();
+        int[] arr = {1,23, 24,21,4,24, 4, 4};
+        System.out.println("Minimum deletion required is: " + obj.getMinDel(arr));
+    }
+}
