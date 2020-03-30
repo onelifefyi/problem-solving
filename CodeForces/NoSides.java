f2368e6bf4a9c0d831d7987175607a50f3147d00
Apoorv <vaporinside@gmail.com>
2020-03-30 14:33:28 +0530

    Added Codeforces div2 600 problem

---
+++ CodeForces/NoSides.java
@@ -0,0 +1,20 @@
+//problem: https://codeforces.com/problemset/problem/785/A
+import java.util.HashMap;
+import java.util.Scanner;
+class NoSides{
+    public static void main(String[] args){
+        HashMap<String, Integer> hMap = new HashMap<>();
+        hMap.put("Tetrahedron", 4);
+        hMap.put("Cube", 6);
+        hMap.put("Octahedron", 8);
+        hMap.put("Dodecahedron", 12);
+        hMap.put("Icosahedron", 20);
+        Scanner sObj = new Scanner(System.in);
+        int noCases = Integer.parseInt(sObj.nextLine());
+        int result = 0;
+        for(int i=0; i<noCases; i++){
+            result += hMap.get(sObj.nextLine());
+        }
+        System.out.print(result);
+    }
+}
