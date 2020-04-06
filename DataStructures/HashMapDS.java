9e05cae1aefb63c077cb910ed4a27683243a00a0
Apoorv <vaporinside@gmail.com>
2020-04-06 09:16:03 +0530

    Removing Algorithms and Data Structures and creating new repo

--- DataStructures/HashMapDS.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-class HashMapDS{
-    public static void main(String[] args){
-        HashMap hm = new HashMap();
-        hm.put("name", "Apoorv");
-        System.out.println((String) hm.get("name"));
-    }
-}
