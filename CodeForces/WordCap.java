577841d07b30be4989818019333409875dcf47be
Apoorv <vaporinside@gmail.com>
2020-04-16 13:02:17 +0530

    WordCap CodeForces 800

---
+++ CodeForces/WordCap.java
@@ -0,0 +1,15 @@
+//problem: https://codeforces.com/problemset/problem/281/A
+
+import java.util.Scanner;
+public class WordCap{
+    public static void main(String[] args){ 
+        Scanner sObj = new Scanner(System.in);
+        String input = sObj.nextLine();
+        if((int)input.charAt(0) < 90){
+            System.out.println(input);
+        }
+        else{
+            System.out.println((char)((int) input.charAt(0) - 32) + input.substring(1,input.length()));
+        }
+    }
+}
