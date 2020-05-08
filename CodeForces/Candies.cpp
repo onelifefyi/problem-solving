7f6ef6b357a16c62459567d080f3dc97921c4f88
v-apor <vaporinside@gmail.com>
2020-05-08 20:21:15 +0530

    CodeForces 800 Solved

--- CodeForces/Candies.cpp
+++ CodeForces/Candies.cpp
@@ -1,4 +1,4 @@
-//problem: https://codeforces.com/problemset/problem/32/A
+//problem: https://codeforces.com/problemset/problem/1230/A
 #include <iostream>
 #include <algorithm>
 int main(void){
@@ -7,11 +7,15 @@ int main(void){
         std::cin >> arr[i];
     }
     std::sort(arr, arr+4);
-    if(arr[0]+arr[3] == arr[1]+arr[2]){
-        std::cout << "YES";
+    if(arr[3] == arr[2] + arr[1] + arr[0]){
+        std::cout<<"YES";
+        return 0;
+    }
+    else if(arr[3] + arr[0] == arr[1] + arr[2]){
+        std::cout<<"YES";
     }
     else{
-        std::cout << "NO";
+        std::cout<<"NO";
     }
     return 0;
 }
