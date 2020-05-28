a4d337f6047d3041ef28252f79e5e3b3085d32bc
Apoorv <vaporinside@gmail.com>
2020-05-28 13:18:42 +0530

    CodeFoeces 900 Planks/Ladders Solved + update notes

--- CodeForces/Planks.java
+++ CodeForces/Planks.java
@@ -1,5 +1,32 @@
 //problem: https://codeforces.com/problemset/problem/1197/A
 
+// import java.util.Arrays;
+// import java.util.Scanner;
+// import java.lang.Math;
+
+// NOT EFFICIENT
+// public class Planks{
+//     public static void main(String[] args){
+//         Scanner sObj = new Scanner(System.in);
+//         int nos = sObj.nextInt();
+//         for(int z=0; z<nos; z++){
+//             int size = sObj.nextInt();
+//             int[] arr = new int[size];
+//             for(int i=0; i<size; i++){
+//                 arr[i] = sObj.nextInt();
+//             }
+//             if(size < 3){
+//                 System.out.println(0);
+//                 continue;
+//             }
+//             Arrays.sort(arr);
+//             System.out.println(Math.min(arr[size-2]-1, size-2));
+//         }
+//     }
+// }
+
+
+// EFFICIENT
 import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.Math;
@@ -9,17 +36,26 @@ public class Planks{
         Scanner sObj = new Scanner(System.in);
         int nos = sObj.nextInt();
         for(int z=0; z<nos; z++){
+            int first, second;
+            first = second = Integer.MIN_VALUE;
             int size = sObj.nextInt();
             int[] arr = new int[size];
             for(int i=0; i<size; i++){
                 arr[i] = sObj.nextInt();
+                if(arr[i] >= first){
+                    second = first;
+                    first = arr[i];
+                }
+                else if(arr[i] > second && arr[i] != first){
+                second = arr[i]; 
+                }
             }
+            //System.out.println("max: " + first + " 2nd max :" + second);
             if(size < 3){
                 System.out.println(0);
                 continue;
             }
-            Arrays.sort(arr);
-            System.out.println(Math.min(arr[size-2]-1, size-2));
+            System.out.println(Math.min(second-1, size-2));
         }
     }
 }
