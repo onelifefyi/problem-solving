26aa9acf7a4acd84c6191c004551fbdecc5353eb
Apoorv <vaporinside@gmail.com>
2020-06-30 07:53:58 +0530

    CTCI + Learn BitSets (for better space)

---
+++ CTCI/arrays-strings/ChangedIsUnique.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+//import java.util.BitSet;
+
+class IsUnique{
+
+    static boolean isUnique(String txt){            // O(N^2)
+        char chr;
+        boolean result = true;
+        for(int i=0; i<txt.length(); i++){
+            chr = txt.charAt(i);
+            for(int j=i+1; j<txt.length(); j++){
+                if(chr == txt.charAt(j)){
+                    result = false;
+                }
+            }
+ //           System.out.print(txt.charAt(i));
+        }
+        return result;
+    }
+
+//Using Hash Map
+    static boolean isUniqueImproved(String txt){
+        HashMap mapH = new HashMap();               
+        for(int i=0; i<txt.length(); i++){              //O(N)
+            if(mapH.containsKey(txt.charAt(i))){
+                return false;
+            }
+            else{
+                mapH.put(txt.charAt(i), 1);
+            }
+        }
+        return true;
+    }
+
+    static boolean isUniqueImproved2(String txt){
+        boolean[] bArr = new boolean[26];
+        for(char c: txt.toCharArray()){      //O(N), Not really improvement, just another approach
+ //           System.out.println(c);
+            if(bArr[c-'a']){
+                return false;
+            }
+            else{
+                bArr[c-'a'] = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String args[]){
+        String txt = "helo";
+        System.out.println("Iteratively: " + isUnique(txt));
+        System.out.println("Using Hash Map: " + isUniqueImproved(txt));
+        System.out.println("Using Boolean array: " + isUniqueImproved2(txt) );
+    }
+}
--- CTCI/arrays-strings/IsUnique.java
+++ CTCI/arrays-strings/IsUnique.java
@@ -1,55 +1,41 @@
+import java.util.BitSet;
 import java.util.HashMap;
-//import java.util.BitSet;
-
-class IsUnique{
-
-    static boolean isUnique(String txt){
-        char chr;
-        boolean result = true;
-        for(int i=0; i<txt.length(); i++){
-            chr = txt.charAt(i);
-            for(int j=i+1; j<txt.length(); j++){
-                if(chr == txt.charAt(j)){
-                    result = false;
-                }
-            }
- //           System.out.print(txt.charAt(i));
-        }
-        return result;
-    }
-
-//Using Hash Map
-    static boolean isUniqueImproved(String txt){
-        HashMap mapH = new HashMap();
-        for(int i=0; i<txt.length(); i++){
-            if(mapH.containsKey(txt.charAt(i))){
+public class IsUnique{
+    static boolean unique1(String input){
+        HashMap hMap = new HashMap();
+        for(char ch: input.toCharArray()){
+            if(hMap.containsKey(ch)){
                 return false;
             }
             else{
-                mapH.put(txt.charAt(i), 1);
+                hMap.put(ch, true);
             }
         }
         return true;
     }
 
-    static boolean isUniqueImproved2(String txt){
-        boolean[] bArr = new boolean[26];
-        for(char c: txt.toCharArray()){
- //           System.out.println(c);
-            if(bArr[c-'a']){
+    static boolean unique2(String input){
+        BitSet bSet = new BitSet();
+        for(char ch: input.toCharArray()){          // O(N) time; O(1) space
+            if(bSet.get(ch - 'a')){
                 return false;
             }
             else{
-                bArr[c-'a'] = true;
+                bSet.set(ch - 'a');
             }
         }
         return true;
     }
 
-    public static void main(String args[]){
-        String txt = "hello";
-        System.out.println("Iteratively: " + isUniqueImproved(txt));
-        System.out.println("Using Hash Map: " + isUniqueImproved(txt));
-        System.out.println("Using Boolean array: " + isUniqueImproved2(txt) );
+    public static void main(String[] args){
+        String input = "polish";
+        // Better time: 
+        System.out.println(unique1(input));          // HashMap
+        System.out.println(unique2(input));          // Bit Vector
+
+        // Better space: 
+        // if no additional DS can be used:
+        // 1) We can iterate through the array for each character and compare: O(N^2) time 
+        // 2) We can sort the string O(NlogN), the compare neighbour char O(N) => total O(NlogN)
     }
 }
