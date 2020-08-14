7fd973b85300709c5632f51fd76991309dd32015
Apoorv <vaporinside@gmail.com>
2020-08-14 09:04:01 +0530

    CTCI/STACK-QUEUES StackSort

--- CTCI/stacks-queues/Stack.java
+++ CTCI/stacks-queues/Stack.java
@@ -13,6 +13,24 @@ public class Stack{
     }
 
     int pop(){
+        if(top == -1) return Integer.MAX_VALUE;
         return stk.remove(top--);
+    }
+
+    int getSize(){
+        return top+1;
+    }
+
+    int peek(){
+        if(top == -1) return Integer.MAX_VALUE;
+        return stk.get(top);
+    }
+
+    void showAll(){
+        System.out.print("Bottom->");
+        for(int ele: stk){
+            System.out.print(ele + " ");
+        }
+        System.out.println("\b<-Top");
     }
 }
