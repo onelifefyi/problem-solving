c8db26844ef57f92a56ec2d02da9a3c9da7849a3
Apoorv <vaporinside@gmail.com>
2020-10-21 21:07:43 +0530

    MaxSubArraySum/Kadanes

--- ProblemsLadder/Arrays/8SubArraySum.java
+++ ProblemsLadder/Arrays/8SubArraySum.java
@@ -2,19 +2,34 @@
 Problem: Given an array arr of N integers. Find the contiguous sub-array with maximum sum.
 
 Approach:
->   
+>   The simplest approach is to find the sum of all the possible sub arrays and returning max of them.
+    time O(n^3) | extra space O(1)
+
+>   One optimization to above approach is to find the max sub array sum originating from each element linearly,
+    by keeping track of last sum.
+    time O(n^2) | auxilary space O(1)
+
+>   The optimal approach is Kadane's Algorithm for this problem.
+    keep track of max subarray sum for current element, and compare it with current element maitaining maxSubArray sum,
+    maintain a globalMax variable keeping track of maximum subArray throughout the array
+    time O(n) | auxilary space O(1)
 
 */
 class SubArraySum {
+
+    public static int getMaxSubArraySum(int[] input){
+        int currentSum, globalSum;
+        currentSum = globalSum = input[0];
+        for(int i=1; i<input.length; i++){
+            currentSum = Math.max(currentSum + input[i], input[i]);
+            if(currentSum > globalSum) globalSum = currentSum;
+        }
+        return globalSum;
+    }
+
     public static void main(String[] args) {
         //int input[] = {1, 2, 3, -2, 5};
-        int input[] = {-1, 6, -4, 2, -5};
-        int maxIndex = 0;
-        int sum = input[0];
-        for(int i=1; i<input.length; i++){
-            if(sum + input[i] > sum) maxIndex = i;
-            sum += input[i];
-        }
-        System.out.println(maxIndex);
+        int input[] = {-1, 6, -4, 5, -5};
+        System.out.println(getMaxSubArraySum(input));
     }    
 }
---
+++ ProblemsLadder/Arrays/9MinimizeHeight.java
@@ -0,0 +1,16 @@
+/*
+Problem:
+Given an array arr[] denoting heights of N towers and a positive integer K, 
+modify the heights of each tower either by increasing or decreasing them by K only once. 
+Find out the minimum difference of the heights of shortest and longest modified towers.
+
+Approach:
+>
+
+*/
+
+class MinimizeHeight {
+    public static void main(String[] args) {
+        
+    }
+}
