58429f88db6e3d419eab6f10acec4828360ba11d
Apoorv <vaporinside@gmail.com>
2020-11-20 21:53:42 +0530

    LeetCode GoodPair: Easy

---
+++ LeetCode/Easy/GoodPair.java
@@ -0,0 +1,17 @@
+// Problem: https://leetcode.com/problems/number-of-good-pairs/
+
+public class GoodPair {
+    public static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++)
+                if(nums[i] == nums[j]) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,1,1,3};
+        System.out.println(numIdenticalPairs(arr));
+    }
+}
