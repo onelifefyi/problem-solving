c5b898b95ff486b476faa68ff5cd75c24a8bd165
Apoorv <vaporinside@gmail.com>
2021-05-07 23:34:13 +0530

    Codeforces 1520/A String Easy

--- CodeForces/Distracted.py
+++ CodeForces/Distracted.py
@@ -1,16 +1,20 @@
 # https://codeforces.com/problemset/problem/1520/A
+# Time O(n) | Space O(1) 
 
-# :INCOMPLETE:
+css = int(input())
+for _ in range(css):
+    freq = [0] * 26
+    suspecious = False
 
+    lenArr = input()
+    arr = [ch for ch in input()]
+    current = arr[0]
 
-freq = [0] * 26
-arr = [ch for ch in input()]
+    for index, ch in enumerate(arr):
+        if freq[ord(ch) - ord('A')] > 0:
+            if arr[index] != arr[index-1]:
+                suspecious = True
+                break
+        freq[ord(ch) - ord('A')] += 1
 
-current = arr[0]
-for index, ch in enumerate(arr):
-    if freq[index] == -1:
-        print("NO")
-        break
-    freq[index] = 1
-    if ch != current:
-        freq[index-1] = -1
+    print("NO" if suspecious else "YES")
