c9f6930d16e3be4c9dd38314f5c951d21b0a68a1
Apoorv <vaporinside@gmail.com>
2021-06-01 23:32:04 +0530

    Pattern +1 With Concept

--- GCI Codemap/Data Structures & Algorithms Java/Milestone4/5Pyra.py
+++ GCI Codemap/Data Structures & Algorithms Java/Milestone4/5Pyra.py
@@ -1,17 +1,34 @@
 # https://www.youtube.com/watch?v=yGCQ9if5I-8&list=PL7ersPsTyYt2prN058WfA_j3ElgwD1bht&index=5
 
 # Done but need to solve with form
-def printPattern(num):
-    start = 1
-    for i in range(num):
-        for j in range(num):
-            # condition in this indendation
-            if j < num-i-1:
-                print(' ',end='')
+# def printPattern(num):
+#     start = 1
+#     for i in range(num):
+#         for j in range(num):
+#             # condition in this indendation
+#             if j < num-i-1:
+#                 print(' ',end='')
 
-        for x in range(0, start):
-            print('*', end='')
-        start += 2
-        print('\n',end='')
+#         for x in range(0, start):
+#             print('*', end='')
+#         start += 2
+#         print('\n',end='')
+
+def printPattern(num):
+    for i in range(num):
+        for j in range(num*2-1):
+            if(j>=num-i-1 and j<num+i):
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print('\n')
 
 printPattern(int(input()))
+
+'''
+....*
+...***
+..*****
+.*******
+*********
+'''
