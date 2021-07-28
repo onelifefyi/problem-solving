89aa1eebfc8613614914640693b411e69b0babbe
Apoorv <vaporinside@gmail.com>
2021-07-28 08:33:04 +0530

    SubArraySort Length Leetcode Med

--- DSA LevelUp/1Arrays/6SubArraySort.py
+++ DSA LevelUp/1Arrays/6SubArraySort.py
@@ -1,10 +1,22 @@
 # https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
-# Above problem modded to return the start and end index
 
 # Approach: Sort the entire array and then compare sorted and unsorted array to find first and last different position
 # Time O(nlogn) | Aux space O(n)
-def findUnsortedSubarray(arr):
-    sortedArr = sorted(arr)
-    first_match = -1
-    last_match = -1
-    
+def findUnsortedSubarray(nums):
+    sortedNums = sorted(nums)
+    lPtr = 0
+    rPtr = len(nums)-1
+    leftmost = 0
+    rightmost = 0
+
+    while(lPtr<len(nums) and nums[lPtr]==sortedNums[lPtr]):
+        lPtr += 1
+        
+    while(rPtr>lPtr and nums[rPtr]==sortedNums[rPtr]):
+        rPtr -= 1
+    return  rPtr-lPtr+1
+
+# Better Approach: Not solved yet
+
+arr = [int(num) for num in input().split()]
+print(findUnsortedSubarray(arr))
