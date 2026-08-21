2bec66d04ddd8ca79847b679671c4e2bb88fb922
Apoorv Chandrakar <apoorv.learn@gmail.com>
2026-08-21 11:37:25 -0400

    0arrays_hashing - fix is valid anagram - reducing one pass

--- NeetCode150/0arrays_hashing/1isAnagram.py
+++ NeetCode150/0arrays_hashing/1isAnagram.py
@@ -8,20 +8,16 @@
 # Better:
 # create a list of size 26 filled with 0s, for each alphabet
 # keep doing +1 for each alphabet in s
-# keep doing -1 for each alphabet in t, if it goes to -1 or at the end is non zero,
-# return False
+# keep doing -1 for each alphabet in t
+# if freq contains non zero element at the end, return False
 # Time O(n) | Space O(1)
 
 def isAnagram(s, t):
     freq = [0] * 26
-    for ch in s:
-        index = ord(ch) - ord('a')
-        freq[index] += 1
-    for ch in t:
-        index = ord(ch) - ord('a')
-        freq[index] -= 1
-        if freq[index] < 0:
-            return False
+    if len(s) != len(t): return False
+    for i in range(len(s)):
+        freq[ord(s[i]) - ord('a')] += 1
+        freq[ord(t[i]) - ord('a')] -= 1
     for num in freq:
         if num != 0: return False
     return True
